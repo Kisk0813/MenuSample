c1d2dd08459182d74d4c8311515aa56022e75f7c
Keisuke <16010409@anabuki-college.ac.jp>
2018-06-13 11:45:17 +0900

    ok

--- app/src/main/java/com/websarva/wings/android/menusample/MenuListActivity.java
+++ app/src/main/java/com/websarva/wings/android/menusample/MenuListActivity.java
@@ -18,8 +18,11 @@ import java.util.Map;
 public class MenuListActivity extends AppCompatActivity {
 
     private ListView _lvMenu;
+
     private List<Map<String, Object>> _menuList;
+
     private static final String[] FROM = {"name", "price"};
+
     private static final int[] TO = {R.id.tvMenuName, R.id.tvMenuPrice};
 
     @Override
@@ -27,94 +30,69 @@ public class MenuListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_list);
 
-        _lvMenu = findViewById(R.id.lvMenu);
-        _menuList = createTeishokuList();
 
-        SimpleAdapter adapter = new SimpleAdapter(
-                MenuListActivity.this,
-                _menuList,
-                R.layout.row,
-                FROM,
-                TO);
-        _lvMenu.setAdapter(adapter);
-        _lvMenu.setOnItemClickListener(new ListItemClickListener());
-    }
-                                                                                        a
-    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+
+        private List<Map<String, Object>> createTeishokuList() {
+            List<Map<String, Object>> menuList = new ArrayList<>();
+
+            Map<String, Object> menu = new HashMap<>();
+            menu.put("name", "から揚げ定食");
+            menu.put("price", 800);
+            menu.put("desc", "若鳥のから揚げにサラダ、ご飯とお味噌汁が付きます。");
+            menuList.add(menu);
+
+            menu = new HashMap<>();
+            menu.put("name", "ハンバーグ定食");
+            menu.put("price", 850);
+            menu.put("desc", "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。");
+            menuList.add(menu);
+
+            menu = new HashMap<>();
+            menu.put("name", "生姜焼き定食");
+            menu.put("price", 850);
+            menu.put("desc", "すりおろし生姜を使った生姜焼きにサラダ、ご飯とお味噌汁が付きます。");
+            menuList.add(menu);
+
+            menu = new HashMap<>();
+            menu.put("name", "ステーキ定食");
+            menu.put("price", 1000);
+            menu.put("desc", "国産牛のステーキにサラダ、ご飯とお味噌汁が付きます。");
+            menuList.add(menu);
+
+            menu = new HashMap<>();
+            menu.put("name", "野菜炒め定食");
+            menu.put("price", 750);
+            menu.put("desc", "季節の野菜炒めにサラダ、ご飯とお味噌汁が付きます。");
+            menuList.add(menu);
+
+            menu = new HashMap<>();
+            menu.put("name", "とんかつ定食");
+            menu.put("price", 900);
+            menu.put("desc", "ロースとんかつにサラダ、ご飯とお味噌汁が付きます。");
+            menuList.add(menu);
+
+            return menuList;
+        }
+
 
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Map<String, Object> item = (Map<String, Object>)parenat.getItemAtPosition(position);
 
+            Map<String, Object> item = (Map<String, Object>) parent.getItemAtPosition(position);
             String menuName = (String) item.get("name");
             Integer menuPrice = (Integer) item.get("price");
+
 
             Intent intent = new Intent(MenuListActivity.this, MenuThanksActivity.class);
 
             intent.putExtra("menuName", menuName);
+
             intent.putExtra("menuPrice", menuPrice + "円");
 
             startActivity(intent);
         }
+
     }
 
-    private List<Map<String, Object>> createTeishokuList(){
-        List<Map<String, Object>> menuList = new ArrayList<>();
-
-        Map<String, Object> menu = new HashMap<>();
-        menu.put("name", "から揚げ定食");
-        menu.put("price", 800);
-        menu.put("desc", "若鶏の唐揚げにサラダ、ご飯とお味噌汁がつきます");
-        menuList.add(menu);
-
-        menu = new HashMap<>();
-        menu.put("name", "ハンバーグ定食");
-        menu.put("price", 850);
-        menu.put("desc", "ハンバーグにサラダ、ご飯とお味噌汁がつきます");
-        menuList.add(menu);
-
-        menu = new HashMap<>();
-        menu.put("name", "生姜焼き定食");
-        menu.put("price", 850);
-        menu.put("desc", "生姜焼きにサラダ、ご飯とお味噌汁がつきます");
-        menuList.add(menu);
-
-        menu = new HashMap<>();
-        menu.put("name", "ステーキ定食");
-        menu.put("price", 1000);
-        menu.put("desc", "ステーキにサラダ、ご飯とお味噌汁がつきます");
-        menuList.add(menu);
-
-        menu = new HashMap<>();
-        menu.put("name", "野菜炒め定食");
-        menu.put("price", 750);
-        menu.put("desc", "野菜炒めにサラダ、ご飯とお味噌汁がつきます");
-        menuList.add(menu);
-
-        menu = new HashMap<>();
-        menu.put("name", "とんかつ定食");
-        menu.put("price", 900);
-        menu.put("desc", "とんかつにサラダ、ご飯とお味噌汁がつきます");
-        menuList.add(menu);
-
-        menu = new HashMap<>();
-        menu.put("name", "ミンチカツ定食");
-        menu.put("price", 850);
-        menu.put("desc", "ミンチカツにサラダ、ご飯とお味噌汁がつきます");
-        menuList.add(menu);
-
-        menu = new HashMap<>();
-        menu.put("name", "チキンカツ定食");
-        menu.put("price", 900);
-        menu.put("desc", "チキンカツにサラダ、ご飯とお味噌汁がつきます");
-        menuList.add(menu);
-
-        menu = new HashMap<>();
-        menu.put("name", "コロッケ定食");
-        menu.put("price", 850);
-        menu.put("desc", "コロッケにサラダ、ご飯とお味噌汁がつきます");
-        menuList.add(menu);
-
-        return menuList;
-    }
 }
